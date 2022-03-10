5245f9c10670b3f458af9e6165560c61502b1a28
CoderJuan21 <70552194+CoderJuan21@users.noreply.github.com>
2022-03-09 20:00:50 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,43 +1,57 @@
-let stories = [
+let words = [
     {
-        "inputs": 8,
-        "title": "Let's Go to the Zoo",
-        "story": `Today we went to the zoo! The first thing we saw was a <span class="rep_input">_____</span> <span class="rep_input">_____</span> <span class="rep_input">_____</span>. The zookeeper told us that was normal, except in <span class="rep_input">_____</span>. I had a <span class="rep_input">_____</span> time! Next time, I will remember that if I ever see <span class="rep_input">_____</span> <span class="rep_input">_____</span>, I should <span class="rep_input">_____</span> the other way.`,
-        "words": ["Black", "Gorilla", "Dancing", "Madagascar", "Nice", "White", "Tigers", "Move"]
+        "inputs": 5,
+        "category": "Sports",
+        "words": "Chess"
     },
     {
-        "inputs": 7,
-        "title": "Picnic Time",
-        "story": `On <span class="rep_input">_____</span> we are going on a picnic! I'm going with my <span class="rep_input">_____</span> and my favourite pet <span class="rep_input">_____</span>. For lunch, we will eat <span class="rep_input">_____</span> and drink <span class="rep_input">_____</span>. We will end the day with a <span class="rep_input">_____</span> game of <span class="rep_input">_____</span>.`,
-        "words": ["Sunday", "Aunt", "Dog", "Burgers", "Soft Drinks", "Nice", "Cards"]
+        "inputs": 6,
+        "category": "European Country Name",
+        "words": "France"
     },
-    {
-        "inputs": 12,
-        "title": "Silly Animal Tale",
-        "story": `There once was a <span class="rep_input">_____</span> <span class="rep_input">_____</span> from <span class="rep_input">_____</span>. Nobody knew he was a <span class="rep_input">_____</span> because he had <span class="rep_input">_____</span> fur and ate <span class="rep_input">_____</span> <span class="rep_input">_____</span> each day. He liked to <span class="rep_input">_____</span> and sing <span class="rep_input">_____</span>. Whenever he was <span class="rep_input">_____</span>, he would start speaking <span class="rep_input">_____</span>. Then he would feel <span class="rep_input">_____</span>.`,
-        "words": ["Smelly", "Cat", "California", "Cats", "Blue", "3", "Fishes", "Dance", "Songs", "Sad", "Childishly", "Happy"]
-    }
 ]
 
-var gameOver =false
 
-$(".clickable").click(function(){
-    var correctGuess = false
-    let id = $(this).attr("id")
-    var life = parseInt($("#life").text())
+$(document).ready(function(){
+    fill_blanks()
+})
 
-    for(var i=0;i<randomWOrd.word.length;i++){
-        if(randomWord.word.charAt(i).toLowerCase() == id){
-            if(life > 0 && ($(".fill_blanks").eq(i).html()== "_" || $(".fill_blanks").eq(i).html() == id)){
-                $(".fill_blanks").eq(i).html(id)
-                correctGuess = true
 
-                if($("#blanks").text() === randomWord.word.toLowerCase()){
-                    $("#result").text("You Winn!!")
+function fill_blanks(){
+    var gameOver =false
+    const randomWord = words[Math.floor(Math.random()*words.length)]
+    $("#blanks").empty()
+    for(let i =0;i<randomWord.inputs;i++){
+        let input_html = `<span class="fill_blanks">${i}</span>`
+        $("#blanks").append(input_html)
+    }
+    $("#hint").html(randomWord.category)
+
+    $(".clickable").click(function(){
+        var correctGuess = false
+        let id = $(this).attr("id")
+        var life = parseInt($("#life").text())
+    
+        for(var i=0;i<randomWOrd.word.length;i++){
+            if(randomWord.word.charAt(i).toLowerCase() == id){
+                if(life > 0 && ($(".fill_blanks").eq(i).html()== "_" || $(".fill_blanks").eq(i).html() == id)){
+                    $(".fill_blanks").eq(i).html(id)
                     correctGuess = true
-                    gameOver = true
+    
+                    if($("#blanks").text() === randomWord.word.toLowerCase()){
+                        $("#result").text("You Winn!!")
+                        correctGuess = true
+                        gameOver = true
+                    }
                 }
             }
         }
-    }
-})
+        if(life > 0 && correctGuess != true && gameOver != true){
+            life = life-1
+            $("#life").text(life)
+        }
+        else if(life == 0){
+            $("#result").text("YOU LOSTTTTTTTT")
+        }
+    })
+}
